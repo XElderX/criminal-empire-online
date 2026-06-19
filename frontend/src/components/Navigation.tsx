6100ace1a64ba09e9b1da2a871c133cfd575d769
XElderX <dalius.badley@gmail.com>
2026-06-19 12:39:04 +0300

    removed overpowered legacy quick crimes

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -33,7 +33,7 @@ export function Navigation({
     <nav className="nav">
       <div className="brand">
         Criminal Empire
-        <span className="version-badge">v0.4.0</span>
+        <span className="version-badge">v0.4.1</span>
       </div>
 
       <div className="nav-links">
